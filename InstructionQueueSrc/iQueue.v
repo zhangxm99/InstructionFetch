a80539a65fc223057b04d9a022ec994873975c85
zhangxm99 <503894188@qq.com>
2024-01-26 17:58:04 +0800

    bug fix

--- InstructionQueueSrc/iQueue.v
+++ InstructionQueueSrc/iQueue.v
@@ -2,14 +2,13 @@
 `define BUFFERSIZE 10
 
 module iQueue(
-    input fire,
     input i_drive,
     input rst,
     input i_freeNext,
     input[7:0] i_cutPostion_8,
     input[64*10-1:0] i_alignedInstructionTable,
-    //第几个b(从0开始)预测为跳，如果全不跳则为-1
-    input[7:0] ithbJump,
+    //是否有预测跳转，有的话一定在截断位置处填上预测的跳转地址就行了
+    input gotJump,
     input[31:0] jumpAddr,
     output[7:0] o_room,
     output [96*4-1:0] o_predictAndPCAndInstr,
@@ -23,20 +22,20 @@ module iQueue(
     assign o_room = room;
     assign o_predictAndPCAndInstr = buffer[96*4-1:0];
 
-    // wire fire;
-    // cFifo1 cFifo1(.i_drive(i_drive),
-    //               .rst(rst),
-    //               .i_freeNext(i_freeNext),
-    //               .o_free(o_free),
-    //               .o_driveNext(o_driveNext),
-    //               .o_fire_1(fire));
+    wire fire;
+    cFifo1 cFifo1(.i_drive(i_drive),
+                  .rst(rst),
+                  .i_freeNext(i_freeNext),
+                  .o_free(o_free),
+                  .o_driveNext(o_driveNext),
+                  .o_fire_1(fire));
 
     genvar i;
     wire[96*`BUFFERSIZE-1:0] tmp, final;
     generate    
         for(i = 0;i < `BUFFERSIZE;i = i+1)begin
             assign tmp[i*96 +: 64] = i_alignedInstructionTable[i*64+:64];
-            assign tmp[i*96+64 +: 32] = ithbJump == i?jumpAddr:0;
+            assign tmp[i*96+64 +: 32] = gotJump == 1&&i_cutPostion_8 == i?jumpAddr:0;
         end
         assign final = (tmp << (9-i_cutPostion_8)*96) >> (9-i_cutPostion_8)*96;
     endgenerate
@@ -64,7 +63,7 @@ module iQueue(
             else begin
                 room <= room + `ISSUEWIDTH - (i_cutPostion_8 + 1);
                 num <= num - `ISSUEWIDTH + (i_cutPostion_8 + 1);
-                buffer <= (buffer >> (96*`ISSUEWIDTH));//newbuffer;
+                buffer <= newbuffer;
             end
 
         end
