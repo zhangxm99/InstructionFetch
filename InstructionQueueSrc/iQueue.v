13de86d0a2846ddfc885f1caace851401cfbc5ec
zhangxm99 <503894188@qq.com>
2024-02-04 22:11:04 +0800

    change out to 64*4

--- InstructionQueueSrc/iQueue.v
+++ InstructionQueueSrc/iQueue.v
@@ -1,5 +1,6 @@
 `define ISSUEWIDTH 4
 `define BUFFERSIZE 10
+`define TABLESIZE 10
 
 module iQueue(
     input i_drive,
@@ -7,20 +8,17 @@ module iQueue(
     input i_freeNext,
     input[7:0] i_cutPostion_8,
     input[64*10-1:0] i_alignedInstructionTable,
-    //是否有预测跳转，有的话一定在截断位置处填上预测的跳转地址就行了
-    input gotJump,
-    input[31:0] jumpAddr,
     output[7:0] o_room,
-    output [96*4-1:0] o_predictAndPCAndInstr,
+    output [96*4-1:0] o_PCAndInstr,
     output o_driveNext,
     output o_free
 );
 
-    reg [32*3*`BUFFERSIZE:0] buffer;
+    reg [32*2*`BUFFERSIZE:0] buffer;
     //剩余可容纳条数、目前有的条数
     reg [7:0] room, num;
     assign o_room = room;
-    assign o_predictAndPCAndInstr = buffer[96*4-1:0];
+    assign o_PCAndInstr = buffer[64*4-1:0];
 
     wire fire;
     cFifo1 cFifo1(.i_drive(i_drive),
@@ -31,20 +29,14 @@ module iQueue(
                   .o_fire_1(fire));
 
     genvar i;
-    wire[96*`BUFFERSIZE-1:0] tmp, final;
-    generate    
-        for(i = 0;i < `BUFFERSIZE;i = i+1)begin
-            assign tmp[i*96 +: 64] = i_alignedInstructionTable[i*64+:64];
-            assign tmp[i*96+64 +: 32] = gotJump == 1&&i_cutPostion_8 == i?jumpAddr:0;
-        end
-        assign final = (tmp << (9-i_cutPostion_8)*96) >> (9-i_cutPostion_8)*96;
-    endgenerate
+    wire[64*`TABLESIZE-1:0] final;
+    assign final = (i_alignedInstructionTable << (9-i_cutPostion_8)*64) >> (9-i_cutPostion_8)*64;
 
-    wire[32*3*`BUFFERSIZE-1:0] newbuffer;
+    wire[32*2*`BUFFERSIZE-1:0] newbuffer;
     //先移位清掉之前的，再加上本轮取得的
-    assign newbuffer = (buffer >> (96*`ISSUEWIDTH)) | (final << (num * 96));
+    assign newbuffer = (buffer >> (64*`ISSUEWIDTH)) | (final << (num * 64));
 
-    wire [96*4-1:0] nops = {96'h13,96'h13,96'h13,96'h13} << (4-(num + i_cutPostion_8 + 1))*96;
+    wire [64*4-1:0] nops = {64'h13,64'h13,64'h13,64'h13} << (4-(num + i_cutPostion_8 + 1))*64;
 
     always @(negedge rst or posedge fire) begin
         if (!rst || i_cutPostion_8 == 8'b11111111 ) begin
@@ -53,7 +45,6 @@ module iQueue(
             buffer <= 0;
         end
         else begin
-            buffer <= newbuffer;
             if(num + i_cutPostion_8 + 1 < `ISSUEWIDTH ) begin
                 num <= 0;
                 room <= `BUFFERSIZE;
